8ebc61cdbe879943bee352d95946512333ab9839
CodexRodney <rodney002mwanje@gmail.com>
2025-04-10 15:59:19 +0300

    feat: completed start screen

--- lib/screens/start_screen/widgets/start_screen.dart
+++ lib/screens/start_screen/widgets/start_screen.dart
@@ -8,25 +8,187 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController roomCodeController = TextEditingController();
+    TextEditingController usernameController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
+
     return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () async {
-              WebSocketChannel channel = await connectToSocket(
-                "ws://127.0.0.1:8080/join-room/6589199356204844921",
-              );
-              if (!context.mounted) return;
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => VibeOthersScreen(channel: channel),
+      body: Center(
+        child: SizedBox(
+          width: 641,
+          height: 1117,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.music_note_sharp, size: 166, color: Color(0xff00C699)),
+              Padding(
+                padding: const EdgeInsets.only(top: 22.0, bottom: 36.0),
+                child: Image.asset("assets/logos/logo.png"),
+              ),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 28.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: TextFormField(
+                          controller: roomCodeController,
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter room code';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: Center(
+                              child: Text(
+                                "Room Code",
+                                style: TextStyle(
+                                  color: Color(0xffA04F51),
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(22),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 28.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: TextFormField(
+                          controller: usernameController,
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter username';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: Center(
+                              child: Text(
+                                "UserName",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color(0xffA04F51),
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(22),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
-            child: Text("Connect To A Server"),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (!formKey.currentState!.validate()) return;
+                          WebSocketChannel channel = await connectToSocket(
+                            "ws://127.0.0.1:8080/join-room/${roomCodeController.text.trim()}",
+                          );
+                          if (!context.mounted) return;
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder:
+                                  (context) =>
+                                      VibeOthersScreen(channel: channel),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xffF00055),
+                          fixedSize: Size(210, 59),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                        ),
+                        child: Text(
+                          "Join",
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff00C699),
+                          fixedSize: Size(210, 59),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                        ),
+                        child: Text(
+                          "Create Room",
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 22.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffA04F51),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width * 0.34,
+                      59,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                  child: Text(
+                    "I Just Want To Vibe Alone",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
